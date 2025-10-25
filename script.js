233e991128e413c94d7483f89bb13f2b36590b0a
Sinz Z <81665412+MichaelAssasin09@users.noreply.github.com>
2025-10-25 09:56:34 +0700

    Refactor navbar and slider functionality

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ const totalCards = cards.length;
 const cardWidth = cards[0].offsetWidth + 16; // Including margin
 
 document.querySelector('.next').addEventListener('click', function() {
-    if (currentIndex < totalCards * 4) {
+    if (currentIndex < totalCards * 8) {
         currentIndex++;
         slider.style.transform = `translateX(-${currentIndex * cardWidth * 0.25}px)`;
     }
@@ -47,6 +47,7 @@ document.querySelector('.show-more-btn').addEventListener('click', function() {
     });
 
 });
+
 
 
 
