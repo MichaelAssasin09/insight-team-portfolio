e46257619e57ac7ec4aa1843d54b7adbbb11353b
Sinz Z <81665412+MichaelAssasin09@users.noreply.github.com>
2025-10-25 10:05:28 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,16 +26,16 @@ const totalCards = cards.length;
 const cardWidth = cards[0].offsetWidth + 16; // Including margin
 
 document.querySelector('.next').addEventListener('click', function() {
-    
+    if (currentIndex < totalCards * 16) {
         currentIndex++;
-        slider.style.transform = `translateX(-${currentIndex * cardWidth * 0.35}px)`;
-    
+        slider.style.transform = `translateX(-${currentIndex * cardWidth * 0.45}px)`;
+    }
 });
 
 document.querySelector('.prev').addEventListener('click', function() {
     if (currentIndex > 0) {
         currentIndex--;
-        slider.style.transform = `translateX(-${currentIndex * cardWidth * 0.35}px)`;
+        slider.style.transform = `translateX(-${currentIndex * cardWidth * 0.45}px)`;
     }
 });
 
@@ -47,6 +47,7 @@ document.querySelector('.show-more-btn').addEventListener('click', function() {
     });
 
 });
+
 
 
 
